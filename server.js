3f8cd7c63f1379c1b2111d0324ce51a2dc688df1
Thapelo Somo <edwardthapelo55@gmail.com>
2024-10-23 11:13:32 +0200

    Imported the api.js to the server.js

--- server.js
+++ server.js
@@ -2,13 +2,14 @@ import "dotenv/config"
 import express from "express"
 import cors from "cors"
 import connectDB from "./config/database.js";
+import router from "./routes/api.js";
 
 const app = express();
 const PORT = process.env.PORT || 8080
 
 app.use(express.json())
 app.use(cors());
-
+app.use("/api", router)
 
 // MongoDB connection with the .env file
 connectDB()
