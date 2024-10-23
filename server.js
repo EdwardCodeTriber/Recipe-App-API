8aa48235cb50ebc17a4820f5937068758e5b5706
Thapelo Somo <edwardthapelo55@gmail.com>
2024-10-23 10:29:28 +0200

    Imported the Cluster connection and it works

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import "dotenv/config"
 import express from "express"
 import cors from "cors"
+import connectDB from "./config/database.js";
 
 const app = express();
 const PORT = process.env.PORT || 8080
@@ -8,5 +9,7 @@ const PORT = process.env.PORT || 8080
 app.use(express.json())
 app.use(cors());
 
+// MongoDB connection with the .env file
+connectDB()
 
 app.listen(PORT, ()=> console.log(`Server running ${PORT}`))
